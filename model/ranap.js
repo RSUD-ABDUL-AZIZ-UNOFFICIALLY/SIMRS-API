2052b4568edbe2d10009db9fe9a5ad13be2f00a8
Ahmat Junaidi <ahmatjunaidi09@gmail.com>
2023-01-16 15:02:06 +0700

    perubahan scrip pada sql

--- model/ranap.js
+++ model/ranap.js
@@ -1,7 +1,7 @@
 const { con } = require('./index.js');
 var getLaporan = async function (from, until) {
     var sql = `
-	SELECT
+	SELECT DISTINCT
 	reg_periksa.no_rawat, 
 	reg_periksa.no_rkm_medis, 
 	pasien.no_ktp, 
@@ -13,6 +13,7 @@ var getLaporan = async function (from, until) {
 	pasien.pnd, 
 	DATE_FORMAT(pasien.tgl_lahir, "%d-%m-%Y") AS tgl_lahir, 
 	pasien.umur, 
+	pasien.jk AS jk,
 	pasien.alamat, 
 	pasien.no_tlp, 
 	kelurahan.nm_kel, 
@@ -22,17 +23,21 @@ var getLaporan = async function (from, until) {
 	DATE_FORMAT(reg_periksa.tgl_registrasi, "%d-%m-%Y") AS tgl_registrasi, 
 	reg_periksa.kd_poli, 
 	dokter.nm_dokter, 
-	kamar_inap.tgl_masuk, 
+	DATE_FORMAT(kamar_inap.tgl_masuk, "%d-%m-%Y") AS tgl_masuk, 
 	kamar_inap.jam_masuk, 
-	kamar_inap.tgl_keluar, 
+	DATE_FORMAT(kamar_inap.tgl_keluar, "%d-%m-%Y") AS tgl_keluar, 
+	kamar_inap.jam_keluar, 
 	kamar_inap.diagnosa_akhir, 
 	kamar_inap.diagnosa_awal, 
-	kamar_inap.jam_keluar, 
 	kamar_inap.ttl_biaya, 
 	kamar_inap.stts_pulang, 
 	kamar_inap.kd_kamar, 
 	bangsal.nm_bangsal, 
-	kamar.kelas
+	reg_periksa.stts_daftar AS kunjungan,
+	kamar.kelas,
+	poliklinik.nm_poli AS masuk_melalui, 
+	suku_bangsa.nama_suku_bangsa, 
+	penjab.png_jawab
 FROM
 	reg_periksa
 	INNER JOIN
@@ -70,12 +75,82 @@ FROM
 	INNER JOIN
 	bangsal
 	ON 
-		kamar.kd_bangsal = bangsal.kd_bangsal 
+		kamar.kd_bangsal = bangsal.kd_bangsal
+	INNER JOIN
+	poliklinik
+	ON 
+		reg_periksa.kd_poli = poliklinik.kd_poli
+	INNER JOIN
+	suku_bangsa
+	ON 
+		pasien.suku_bangsa = suku_bangsa.id
+	INNER JOIN
+	penjab
+	ON 
+		pasien.kd_pj = penjab.kd_pj AND
+		reg_periksa.kd_pj = penjab.kd_pj
 	WHERE
-	reg_periksa.tgl_registrasi BETWEEN ? AND ?
+	reg_periksa.tgl_registrasi BETWEEN ? AND ? AND
+	reg_periksa.status_lanjut = 'Ranap' AND
+	kamar_inap.stts_pulang NOT LIKE 'Pindah Kamar'
 		`;
+
+    // const result = await con.query(sql, [from, until]);
+    // return result;
+	var sql2 = `
+SELECT DISTINCT
+	diagnosa_pasien.kd_penyakit AS kode_icd10, 
+	diagnosa_pasien.no_rawat, 
+	diagnosa_pasien.status_penyakit AS kasus,
+	penyakit.nm_penyakit AS deskripsi_icd10
+FROM
+	diagnosa_pasien
+	INNER JOIN
+	penyakit
+	ON 
+		diagnosa_pasien.kd_penyakit = penyakit.kd_penyakit
+WHERE
+	diagnosa_pasien.no_rawat = ?
+ORDER BY
+	diagnosa_pasien.prioritas ASC
+LIMIT 3`;
+
+var sql3 = `
+SELECT DISTINCT
+	prosedur_pasien.kode AS kode_icd9, 
+	icd9.deskripsi_pendek AS deskripsi_icd9, 
+	prosedur_pasien.no_rawat, 
+	reg_periksa.no_reg
+FROM
+	prosedur_pasien
+	LEFT JOIN
+	icd9
+	ON 
+		prosedur_pasien.kode = icd9.kode
+	INNER JOIN
+	reg_periksa
+	ON 
+		prosedur_pasien.no_rawat = reg_periksa.no_rawat
+WHERE
+	prosedur_pasien.no_rawat = ? 
+ORDER BY
+	prosedur_pasien.prioritas ASC
+LIMIT 1`;
+
+	var a = [];
+	var valueToPush = [];
     const result = await con.query(sql, [from, until]);
-    return result;
+	
+	for (const iterator of result) {
+		const icd10 = await con.query(sql2, [iterator.no_rawat]);
+		const icd9 = await con.query(sql3, [iterator.no_rawat]);
+		valueToPush=iterator
+		valueToPush['icd10']=icd10
+		valueToPush['icd9']=icd9
+		a.push(valueToPush)
+	}
+	// console.log(a);
+    return a;
 };
 
 module.exports = {
